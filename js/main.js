5732b136fffd3493faf8a869b773f9c1883b5820
Syrimande <caterinadasilva@gmail.com>
2017-05-08 20:18:17 -0300

    Funcionalidad y Estilo de Calculadora IMC

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-alert("calculadora");
+function calculaIMC(){
+	var calculadora = document.getElementById("calculadora");
+	var peso = document.getElementById("peso").value;
+	var altura = document.getElementById("altura").value;
+	if( peso != "" && altura != "" ){
+		altura = parseInt(altura) / 100;
+		var imc = peso / (altura * altura);
+		calculadora.innerHTML += '<p>Con una altura de ' + altura + 'm y un peso de ' + peso + 'kgs, tu índice de masa corporal es <span>' + imc.toFixed(2) + '</span>.</p>';
+	}
+	peso = "";
+	altura = "";
+}
