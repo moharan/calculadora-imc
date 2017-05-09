9f44b18fd97f637d0ace8b6ef5dd199a55f3d57a
camila-frangp <camila.frangp@gmail.com>
2017-05-09 11:56:10 -0300

    funcion imc lista

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-alert("calculadora");
+function imcCalcular(){
+  var save = document.getElementById("texto");
+  var peso = document.getElementById("kg").value;
+  var estatura = document.getElementById("cm").value;
+  var altura = Math.pow(estatura/100,2).toFixed(2);
+  var imc = peso/altura;
+  console.log(imc);
+  save.innerHTML = "Tienes un índice de: "+ imc;
+}
+imcCalcular();
+
